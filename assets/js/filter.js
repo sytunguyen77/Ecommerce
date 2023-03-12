778b77c1262b1774512ea7b2bea84d7911351d2a
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2023-03-12 10:26:19 -0500

    Add filter function

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,5 +1,57 @@
-const searchShop = document.getElementById("search-input");
-const productsDiv = document.getElementById("products");
+// Define a message for when there are no results
+const noResultsMessage = "No results found";
 
-// Function to display the products
-const displayProducts = (searchShop) => {};
+// Function to display the products or a message if there are no results
+const displayProductsOrMessage = (productsToShow) => {
+   const productList = document.querySelector(".product-list");
+   productList.innerHTML = "";
+
+   if (productsToShow.length > 0) {
+      productsToShow.forEach((product) => {
+         productsEl2.innerHTML += `
+            <article class="products__card">
+                     <img src="${product.imgSrc}" alt="" class="products__img" />
+
+                     <h3 class="products__title">${product.name}</h3>
+                     <span class="products__price">$${product.price}</span>
+
+                     <button class="products__button" onclick="addToCart2(${product.id})">
+                        <i class="bx bx-shopping-bag"></i>
+                     </button>
+
+                     <button class="products-wishlist__button" onclick="addToWishList(${product.id})">
+                        <i class="bx bx-heart"></i>
+                     </button>
+                     
+            </article>
+         `;
+      });
+   } else {
+      const noResultsDiv = document.createElement("div");
+      noResultsDiv.classList.add("no-results");
+      noResultsDiv.textContent = noResultsMessage;
+      noResultsDiv.style.position = "absolute";
+      noResultsDiv.style.left = "50%";
+      noResultsDiv.style.transform = "translateX(-50%)";
+      productList.appendChild(noResultsDiv);
+   }
+};
+
+// Call the displayProductsorMessage function to show all products initially
+displayProductsOrMessage(products2);
+
+// Add an event listener to the search input field
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("keyup", () => {
+   let filter = searchInput.value.toUpperCase();
+   let productsToShow = products2.filter((product) => product.name.toUpperCase().includes(filter));
+   displayProductsOrMessage(productsToShow);
+});
+
+// Add filter to products section
+const searchProduct = document.getElementById("find");
+searchProduct.addEventListener("keyup", () => {
+   let filter = searchProduct.value.toUpperCase();
+   let productsToShow = products2.filter((product) => product.name.toUpperCase().includes(filter));
+   displayProductsOrMessage(productsToShow);
+});
