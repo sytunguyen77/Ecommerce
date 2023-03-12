3c256a4546d9cc9f26d55c2799e5467766d26f9d
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2023-03-11 19:20:35 -0600

    Update filter.js

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,3 +1,5 @@
 const searchShop = document.getElementById("search-input");
+const productsDiv = document.getElementById("products");
 
 // Function to display the products
+const displayProducts = (searchShop) => {};
