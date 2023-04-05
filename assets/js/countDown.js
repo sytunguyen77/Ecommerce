e12386b25c8fc80c9d26c75c8bf274c38ea15b12
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2023-04-05 01:26:52 -0500

    update html

--- assets/js/countDown.js
+++ assets/js/countDown.js
@@ -1,13 +1,13 @@
-var countDownDate = new Date("April 30, 2023 00:00:00").getTime();
-var x = setInterval(function () {
-   var now = new Date().getTime();
+const countDownDate = new Date("April 30, 2023 00:00:00").getTime();
+const x = setInterval(function () {
+   const now = new Date().getTime();
    //  New Date - Today's date
-   var distance = countDownDate - now;
+   const distance = countDownDate - now;
 
-   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
    document.getElementById("days").innerHTML = days;
    document.getElementById("hours").innerHTML = hours;
